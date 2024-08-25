1f4caaa6a6c7e81c7f9e17f12977b97e240ac4ec
Chi Vinh Duong <vinh.duong@genestory.ai>
2024-08-25 23:15:24 +0700

    push dataset

---
+++ src/train.py
@@ -0,0 +1,29 @@
+import torch
+import argparse
+
+from utils.ultrasoundDataset import UltraSoundDataset
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Subset
+
+def train_val_dataset(dataset, val_split=0.25):
+    train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=val_split)
+    datasets = {}
+    datasets['train'] = Subset(dataset, train_idx)
+    datasets['test'] = Subset(dataset, val_idx)
+    return datasets
+
+
+
+def train():
+    # Init parameter 
+    data_path = './data/Dataset_BUSI_with_GT'
+
+    # Load dataset
+    ultraSoundDataset = UltraSoundDataset(data_path)
+    ultraSoundDataset = train_val_dataset(ultraSoundDataset, 0.2)
+    print(len(ultraSoundDataset['train']))
+    print(len(ultraSoundDataset['test']))
+
+
+if __name__ == '__main__':
+    train()
---
+++ src/utils/ultrasoundDataset.py
@@ -0,0 +1,42 @@
+import torch
+import cv2
+import os
+
+import numpy as np
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
+class UltraSoundDataset(Dataset):
+    def __init__(self, root="./data/Dataset_BUSI_with_GT", transform=None):
+        # Initialize dataset paths and parameters
+        self.root = root
+        self.img_names = []
+        self.transform = transform
+        self.categories = ['normal', 'benign', 'malignant']
+        self.labels = []
+        
+        # Collect image names and labels for each category
+        for idx, cat in enumerate(self.categories):
+            cat_path = os.path.join(self.root, cat)
+            files = [f.replace(".png", "") for f in os.listdir(cat_path) if "mask" not in f]
+            self.img_names.extend(files)
+            self.labels.extend([idx] * len(files))
+            
+    def __len__(self):
+        # Return dataset size
+        return len(self.img_names)
+    
+    def __getitem__(self, item):
+        # Load image and mask
+        img_name, label = self.img_names[item], self.labels[item]
+        img = cv2.imread(os.path.join(self.root, self.categories[label], img_name + ".png"), cv2.IMREAD_COLOR)
+        mask = cv2.imread(os.path.join(self.root, self.categories[label], img_name + "_mask.png"), cv2.IMREAD_GRAYSCALE)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # Convert mask values from 255 (default) to 1 for binary 
+        mask[mask == 255] = 1
+        # apply transformations
+        if self.transform:
+            augmented = self.transform(image=img, mask=mask)
+            img, mask = augmented['image'], augmented['mask']
+        
+        return img, mask
